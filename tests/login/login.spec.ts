b12fda40d7290303466b20dbbf2463f55b62e8de
p2635 <4522927+p2635@users.noreply.github.com>
2026-09-02 16:10:22 +0100

    feat: add new page object test for login failure

--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -1,6 +1,9 @@
 import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../pages/loginPage";
 
-test("show login error on wrong email and password", async ({ page }) => {
+test("(non-page object) show login error on wrong email and password", async ({
+  page,
+}) => {
   await page.goto("/");
   await page.getByTestId("nav-sign-in").click();
   await page.getByTestId("email").fill("fake@fake.com");
@@ -8,6 +11,17 @@ test("show login error on wrong email and password", async ({ page }) => {
 
   await page.getByTestId("login-submit").click();
 
+  await expect(page.getByTestId("login-error")).toContainText(
+    "Invalid email or password",
+  );
+});
+
+test("(page object) show login error on wrong email and password", async ({
+  page,
+}) => {
+  const loginPage = new LoginPage(page);
+  loginPage.goto();
+  loginPage.logIn("fake@fake.com", "fakepass");
   await expect(page.getByTestId("login-error")).toContainText(
     "Invalid email or password",
   );
