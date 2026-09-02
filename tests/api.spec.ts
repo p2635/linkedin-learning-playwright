2bc5b19a2de94482dbb88353776f0541535cd9d5
p2635 <4522927+p2635@users.noreply.github.com>
2026-09-02 13:03:57 +0100

    refactor: define API urls as const

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -5,8 +5,10 @@ const EXPECTED_COUNT_PRODUCT_ITEMS = 9;
 const EXPECTED_COUNT_PRODUCT_ITEMS_TOTAL = 50;
 
 test.describe("Products API", async () => {
+  const API_URL = API_BASE_URL + "/products";
+
   test("GET test", async ({ request }) => {
-    const RESPONSE = await request.get(API_BASE_URL + "/products");
+    const RESPONSE = await request.get(API_URL);
     const BODY = await RESPONSE.json();
 
     expect(RESPONSE.status()).toBe(200);
@@ -16,11 +18,12 @@ test.describe("Products API", async () => {
 });
 
 test.describe("Login", async () => {
+  const API_URL = API_BASE_URL + "/users/login";
   const USER = "customer2@practicesoftwaretesting.com";
   const PASS = "welcome01";
 
   test("POST test", async ({ request }) => {
-    const RESPONSE = await request.get(API_BASE_URL + "/users/login", {
+    const RESPONSE = await request.get(API_URL, {
       data: {
         email: USER,
         password: PASS,
