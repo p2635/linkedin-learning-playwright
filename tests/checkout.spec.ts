ae6cb1c45604271738fc11776dbffb366a22cf4f
p2635 <4522927+p2635@users.noreply.github.com>
2026-09-02 13:31:38 +0100

    feat: add checkout happy path test

---
+++ tests/checkout.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Checkout any item as guest happy path", () => {
+  test("test", async ({ page }) => {
+    await page.goto("/");
+
+    // Add item to cart
+    await page.getByAltText("Combination Pliers").click();
+    await page.getByTestId("add-to-cart").click();
+
+    // Navigate to cart and go through payment process
+    await page.getByTestId("nav-cart").click();
+    await page.getByTestId("proceed-1").click();
+    await page.getByRole("tab", { name: "Continue as Guest" }).click();
+    await page.getByTestId("guest-email").fill("my@email.com");
+    await page.getByTestId("guest-first-name").fill("John");
+    await page.getByTestId("guest-last-name").fill("Doe");
+    await page.getByTestId("guest-submit").click();
+    await page.getByTestId("proceed-2-guest").click();
+    await page.getByTestId("country").selectOption("AL");
+    await page.getByTestId("postal_code").fill("5555");
+    await page.getByTestId("house_number").fill("55");
+    await page.getByTestId("proceed-3").click();
+    await page.getByTestId("payment-method").selectOption("buy-now-pay-later");
+    await page.getByTestId("monthly_installments").selectOption("3");
+    await page.getByTestId("finish").click();
+
+    await expect(
+      page.locator("div").filter({ hasText: "Payment was successful" }).nth(5),
+    ).toBeVisible();
+    await expect(page.getByTestId("payment-success-message")).toContainText(
+      "Payment was successful",
+    );
+  });
+});
